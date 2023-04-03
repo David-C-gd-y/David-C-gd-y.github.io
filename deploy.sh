0bc4e48690717adaf3d7f73d6883ad5683077b57
fu <xiao>
2023-04-03 16:45:11 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# abort on errors
 set -e
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
+# navigate into the build output directory
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:David-C-gd-y/vueperssdemo.git main
+# deploy to https://<USERNAME>.github.io
+git push -f git@github.com:{username}/{username}.github.io.git master
 
 cd -
